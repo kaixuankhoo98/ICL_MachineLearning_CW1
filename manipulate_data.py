0c452a74f151eee69609129302c03787b783880c
Kaixuan Khoo <kaixuan.khoo.98@gmail.com>
2022-01-30 23:51:50 +0000

    Added function to separate by a certain letter only_certain_y

--- manipulate_data.py
+++ manipulate_data.py
@@ -4,19 +4,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-(x, y, classes) = rd.read_dataset("intro2ml_cw1_13/data/train_full.txt")
+(x, y, classes) = rd.read_dataset("intro2ml_cw1_13/data/toy.txt")
 
 print("For train_sub.txt")
 rd.display_data_information(x,y,classes)
-new_list_x, new_list_y = rd.sort_list_by_attribute(x,y,0)
-print(new_list_x, new_list_y)
-plt.figure()
-plt.title("Overall distribution of each dataset")
-plt.hist(x)
-plt.xlabel("Attribute Value")
-plt.ylabel("Frequency")
-plt.show()
-# rd.understand_attributes(x)
+
+# new_list_x, new_list_y = rd.sort_list_by_attribute(x,y,0)
+# print(new_list_x, new_list_y)
+
+new_x_onlyA = rd.only_certain_y(x,y,0)
+new_x_onlyC = rd.only_certain_y(x,y,1)
+print(new_x_onlyA)
+print(new_x_onlyC)
 
 # class_names = []
 # for letter in classes:
