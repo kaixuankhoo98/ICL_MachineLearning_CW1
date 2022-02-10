5a87e785619525aba04a6fc0ce0e46842293c62b
bq21 <bq21@ic.ac.uk>
2022-02-10 12:17:23 +0000

    Updating classificationV2.py with changes to fit method

--- classificationV2.py
+++ classificationV2.py
@@ -1,10 +1,19 @@
+#############################################################################
+# Introduction to Machine Learning
+# Coursework 1 Skeleton code
+# Prepared by: Josiah Wang
+#
+# Your tasks: Complete the fit() and predict() methods of DecisionTreeClassifier.
+# You are free to add any other methods as needed.
+##############################################################################
+
 import numpy as np
 import max_info_gain as mig
 from node import Node
 
 
-class PostPrunedDecisionTreeClassifier(object):
-    """Improved Decision Tree Classifier.
+class DecisionTreeClassifier(object):
+    """Basic decision tree classifier
 
     Attributes:
     is_trained (bool): Keeps track of whether the classifier has been trained
@@ -14,15 +23,13 @@ class PostPrunedDecisionTreeClassifier(object):
     predict(x): Predicts the class label of samples X
     prune(x_val, y_val): Post-prunes the decision tree
     """
-    DecisionTree = Node(
-            attribute_index=None, attribute_value=None, x=None, y=None, classes=None
-        )
 
     def __init__(self):
         self.is_trained = False
-        # DecisionTree = Node(
-        #     attribute_index=None, attribute_value=None, x=None, y=None, classes=None
-        # ) # moved it outside so it's an attribute
+        DecisionTree = Node(attribute_index=None, attribute_value=None, x=None, y=None, classes=None)
+
+    def traverse_and_print_tree(self):
+        self.DecisionTree.print_tree()
 
     def fit(self, x, y):
         """Constructs a decision tree classifier from data
@@ -46,14 +53,8 @@ class PostPrunedDecisionTreeClassifier(object):
 
         # convert y into y_index and classes to pass into max info gain functions
         [classes, y_index] = np.unique(y, return_inverse=True)
-
-        print("Trying info_gain function...")
         mig.calculate_best_info_gain(x, y_index, classes)
-        print("Now, trying induce decision tree...")
         self.DecisionTree = mig.induce_decision_tree(x, y_index, classes)
-        # print("First node values are:")
-        # print(self.DecisionTree.attribute_index)
-        # print(self.DecisionTree.attribute_value)
         # set a flag so that we know that the classifier has been trained
         self.is_trained = True
 
@@ -61,9 +62,8 @@ class PostPrunedDecisionTreeClassifier(object):
         if not self.is_trained:
             raise Exception("Tree has not been constructed.")
         node_iterator = self.DecisionTree
-        # iterate through the tree.
-        temp_node = self.DecisionTree
-        
+        #TODO: implement iterating through the tree.
+        pass
 
     def predict(self, x):
         """Predicts a set of samples using the trained DecisionTreeClassifier.
@@ -86,43 +86,20 @@ class PostPrunedDecisionTreeClassifier(object):
 
         # set up an empty (M, ) numpy array to store the predicted labels
         # feel free to change this if needed
-        predictions = np.zeros((x.shape[0],), dtype=np.object)
-
-        predictions2 = []
+        predictions = []
 
         #######################################################################
         #                 ** TASK 2.2: COMPLETE THIS METHOD **
         #######################################################################
         for test_instance in x:
-            temp_node = self.DecisionTree
-            count = 0
-            for value in test_instance:
-                if count == temp_node.attribute_index:
-                    if temp_node.last_node == False:
-                        if value <= temp_node.attribute_value:
-                            temp_node = temp_node.left_child
-                        else:
-                            temp_node = temp_node.right_child
-                count += 1
-            predictions2.append(mig.find_majority_label(temp_node.y))
+            node_pointer = self.DecisionTree
+            while(node_pointer.last_node == False):
+                if(test_instance[node_pointer.attribute_index] <= node_pointer.attribute_value):
+                    node_pointer = node_pointer.left_child
+                else:
+                    node_pointer = node_pointer.right_child
+            predictions.append(mig.find_majority_label(node_pointer.y))
 
-        predictions2 = np.array(predictions2)
+        predictions = np.array(predictions)
 
-        # for test_instance in x:
-        #     for attribute_value in test_instance:
-        #         count = 0
-        #         while (
-        #             self.DecisionTree.left_child != None
-        #             or self.DecisionTree.right_child != None
-        #         ):
-        #             count += 1
-        #             print(temp_node.attribute_index)
-        #             if count == temp_node.attribute_index:
-        #                 if attribute_value <= temp_node.attribute_value:
-        #                     ...
-        #                     temp_node = temp_node.left_child
-        #                 else:
-        #                     temp_node = temp_node.right_child
-        #         predictions.append(temp_node.y)
-        # remember to change this if you rename the variable
-        return predictions2
+        return predictions
